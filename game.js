2d3bb04d995b3108ed81661674763f3eff933d22
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-10-13 22:53:27 +0530

    audio added for the buttons

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 //5. At the top of the game.js file, create a new empty array called gamePattern.
 var gamePattern = [];
 
+
+
 //1. Inside game.js create a new function called nextSequence()
 function nextSequence() {
   //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
